88033b8293946221f43e86d95e7535526af87cdf
MarcGasconO <m.gascon@tutamail.com>
2025-09-28 19:18:27 +0200

    0.1

---
+++ Actividad2/pedirYLeer3.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    int num3;
+    int x;
+    
+    printf("Primer numero: ");
+    scanf("%d", &num1);
+    printf("Segundo numero: ");
+    scanf("%d", &num2);
+    printf("Tercer numero: ");
+    scanf("%d", &num3);
+
+    x = num1;
+    if(num2 > x) {
+        x = num2;
+    }
+    if(num3 > x) {
+        x = num3;
+    }
+    printf("El numero mas grande es: %d\n", x);
+    
+}
